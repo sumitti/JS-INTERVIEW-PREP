19fcaadda61a38ff2ae6459a8e8019a7624e644b
Sumit Tiwari <137861481+sumitti@users.noreply.github.com>
2025-01-03 18:35:44 +0000

    learn var,let,const

--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -1,1 +1,13 @@
-console.log("Sumit")
+const emailId = "sumit@gmail.com"
+let password = 12479897
+var username = "sumit-tiwari"
+city = "Mumbai"
+
+// emailId = "at@gmail.com"
+/*emailId = "at@gmail.com"
+TypeError: Assignment to constant variable.*/
+password = 9789153465
+username = "amit-tiwari"
+city = "Jaipur"
+
+console.table([password,username,city])
